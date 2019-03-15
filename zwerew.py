526684f3c85b1d55f1fc8ed707849fc6a5440b9d
zwerewtolian <45330935+zwerewtolian@users.noreply.github.com>
2019-03-15 20:06:51 +0300

    test

--- zwerew.py
+++ zwerew.py
@@ -1,2 +1,9 @@
-import hashlib
-print(hashlib.md5(b"hello, hash!").hexdigest())
+import time
+t = True
+x = 0
+while t:
+    print('test')
+    time.sleep(1)
+    x += 1
+    if x == 10:
+        t = False
